d40f659662b7570dc62615328680fe451b1c3c0c
Daniel Himes <dhimes@gmail.com>
2024-12-01 12:43:26 -0500

    trying to fix version tag, and set this up so I can run it manually

--- website/calls_to_api.py
+++ website/calls_to_api.py
@@ -1,10 +1,10 @@
 import requests
+import os
 
-url = "http://dnd_kuberneties_excuse-dnd_api-1:8000"
+url = os.getenv("API_URL", "http://dnd_kuberneties_excuse-dnd_api-1:8000")
+
 
 def pull_new_town():
     endpoint_url = f"{url}/build_town"
     response = requests.post(endpoint_url, json={"store_type": "combat_store"})
     return response.json()
-
-
